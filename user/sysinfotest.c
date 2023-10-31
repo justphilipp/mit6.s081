804b69f532fe1304d9256376bcde83e830ee7bc5
philipp <bhwangphilipp@gmail.com>
2023-10-31 14:04:53 +0800

    lab 2

--- user/sysinfotest.c
+++ user/sysinfotest.c
@@ -1,13 +1,19 @@
 #include "kernel/types.h"
 #include "user/user.h"
+#include "kernel/sysinfo.h"
 
 int main(int argc, char *argv[]){
-  if (argc != 2) {
+  if (argc != 1) {
 	printf("correct input is : info *sysinfo\n");
 	return -1;
   }
-  struct sysinfo *si;
-  si = (struct sysinfo *)argv[1];
-  sysinfo(si);
+  struct sysinfo si;
+  sysinfo((struct sysinfo *)&si);
+  if (si.nproc == 0) {
+	printf("sysinfo error\n");
+	return -1;
+  }
+  printf("free memory: %d, free proc: %d\n", si.freemem, si.nproc);
+  printf("sysinfotest:OK\n");
   return 0;
 }
